1ecbec325760477b4d7d48e308a67a90a943d32c
Hemant Dhote <hemantdhote76@gmail.com>
2025-02-28 01:48:17 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,74 @@ import Product from './components/product'
 
 const App = () => {
 
+ 
+
   const [data, setdata] = useState([]);
-  const [formdata, setformdata] = useState({
+  const [edit, setedit] = useState(null);
+  const [formData, setformData] = useState({
     name:"",
     discription:"",
     price:"",
     url:""
   });
 
-  const changeHandler=(e)=>{
-    setformdata({...formdata,[e.target.name]:e.target.value})
+
+  const handleChange=(e)=>{
+    setformData({...formData,[e.target.name]:e.target.value})
+
   }
+  
+
 
   const submitHandler=(e)=>{
     e.preventDefault();
-    setdata([...data,formdata]);
-    setformdata({name:"",discription:"",price:"",url:""})
+
+    if(edit !==null){
+      data[edit]=formData;
+      setedit(null)
+    }
+    else{
+      setdata([...data,formData])
+
+    }
+
+   
+    setformData({name:"",discription:"",price:"",url:""})
+
+
+
+  }
+
+  const deleteHandler=(index)=>{
+   const filteredData= data.filter((_,i)=>i !==index)
+    setdata(filteredData)
+  }
+  
+  const updateHandler=(index)=>{
+
+    setformData(data[index])
+    setedit(index)
+
   }
 
   return (
     <div>
 
       <div className='w-full flex  justify-center items-center p-9'>
-        <form onSubmit={submitHandler} className='flex flex-col gap-3'>
+        <form onSubmit={submitHandler}  className='flex flex-col gap-3'>
           <h1 className='text-xl font-bold text-green-500'>Add new product</h1>
-          <input onInput={changeHandler} className='bg-neutral-300 px-4 py-2 rounded-md outline-none w-70' type="text" placeholder='Enter the product name' name='name' value={formdata.name}/>
-          <input onInput={changeHandler} className='bg-neutral-300 px-4 py-2 rounded-md outline-none w-70' type="text" placeholder='Enter the product discription'  name='discription' value={formdata.discription}/>
-          <input onInput={changeHandler} className='bg-neutral-300 px-4 py-2 rounded-md outline-none w-70' type="number" placeholder='Enter the price of the product ' name='price' value={formdata.price}/>
-          <input onInput={changeHandler} className='bg-neutral-300 px-4 py-2 rounded-md outline-none w-70' type="text" placeholder='Enter the URL of the product '  name='url' value={formdata.url}/>
-          <button className='bg-blue-500 px-4 py-2 rounded-md outline-none w-70 text-white' type="text">submit</button>
+          <input onChange={handleChange} className='bg-neutral-300 px-4 py-2 rounded-md outline-none w-70' type="text" placeholder='Enter the product name' name='name' value={formData.name} required />
+          <input onChange={handleChange} className='bg-neutral-300 px-4 py-2 rounded-md outline-none w-70' type="text" placeholder='Enter the product discription'  name='discription' value={formData.discription}  required/>
+          <input onChange={handleChange} className='bg-neutral-300 px-4 py-2 rounded-md outline-none w-70' type="number" placeholder='Enter the price of the product ' name='price' value={formData.price} required/>
+          <input onChange={handleChange} className='bg-neutral-300 px-4 py-2 rounded-md outline-none w-70' type="text" placeholder='Enter the URL of the product '  name='url'  value={formData.url} required/>
+          <button className='bg-blue-500 px-4 py-2 rounded-md outline-none w-70 text-white' type="text">{edit !== null ? "update":"submit"}</button>
         </form>
       </div>
       <div className='p-4 flex gap-4 flex-wrap'>
 
-        {data.map((elem,index)=>(
-          <Product  key={index} />
-        ))}
+       {data.map((elem , index)=>(
+        <Product  key={index} data={elem} idx={index} deleteHandler={deleteHandler}  updateHandler={updateHandler} />
+       ))}
 
         
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 
-const product = () => {
+const product = ({data,deleteHandler,idx,updateHandler}) => {
+ 
+
+  
+  
+  
   return (
     <div>
       <div className="p-3 bg-neutral-500 w-60 rounded-md">
         <div className="h-35 w-full bg-blue-500">
           <img
             className="h-full w-full object-cover object-top"
-            src="https://tse1.mm.bing.net/th?id=OIP.nvdM5sEm4DqG8oZb8nNStQHaHa&pid=Api&rs=1&c=1&qlt=95&w=112&h=112"
-            alt=""
+            src={data.url}
+            alt={data.name}
           />
         </div>
 
-        <h3 className="text-white text-xl font-bold capitalize">smart phone</h3>
+        <h3 className="text-white text-xl font-bold capitalize">{data.name}</h3>
         <p className="mt-1 text-white text-start">
-          Adjustable aluminum stand compatible with all smartphones and tablets.
+          {data.discription}
         </p>
-        <p className="font-bold text-white mt-2"> $15.99</p>
+        <p className="font-bold text-white mt-2"> ${data.price}</p>
         <div className="flex mt-3 gap-5 ">
-          <button className="px-5 py-2 bg-blue-900 rounded-md text-white ">
+          <button onClick={()=>updateHandler(idx)} className="px-5 py-2 bg-blue-900 rounded-md text-white ">
             Edit
           </button>
-          <button className="px-5 py-2 bg-red-500 rounded-md text-white ">
+          <button onClick={()=>deleteHandler(idx)} className="px-5 py-2 bg-red-500 rounded-md text-white ">
             Delete
           </button>
         </div>
